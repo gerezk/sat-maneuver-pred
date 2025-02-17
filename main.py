6f3a00141d0ab2a036c45a7ef3aef48ed8178169
Kyzer Gerez <kyzer.gerez@gmail.com>
2025-02-17 18:51:50 +0100

    switch from json to tle to be more compatible with orekit

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import argparse
 from pathlib import Path
 
-from preprocess import preprocess, get_tle
+from preprocess import preprocess
 from propagate import propagate
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--scc', type=str, default='', help='Five-digit NORAD ID')
     parser.add_argument('--predict', type=int, default=14, help='Number of days to propagate out to')
+    parser.add_argument('--time_step', type=int, default=24, help='Time step in hours for propagation')
     parser.add_argument('--csv', type=bool, default=False, help='Output preprocessed data to CSV')
     args = parser.parse_args()
 
     # Preprocess ELSET
     if not Path('./data/preprocessed').exists():
         Path('./data/preprocessed').mkdir()
-    historical_ELSET = preprocess(args.scc, args.csv)
+    if args.csv: preprocess(args.scc)
 
-    # Propagate TLE
-    propagate(args.scc, args.predict)
+    # # Propagate TLE
+    # propagate(args.scc, args.predict, args.time_step)
--- preprocess.py
+++ preprocess.py
@@ -1,56 +1,112 @@
 import pandas as pd
 import yaml
 from pathlib import Path
+import math
 
-def get_maneuvers(scc):
-    """ pull maneuver data from yaml and return as list
+import orekit
+vm = orekit.initVM()
+from orekit.pyhelpers import setup_orekit_curdir
+setup_orekit_curdir(from_pip_library=True)  # Sets up the data path (e.g., Orekit-data.zip)
+
+# Import necessary Orekit classes
+from org.orekit.propagation.analytical.tle import TLE, TLEPropagator
+from org.orekit.orbits import OrbitType, KeplerianOrbit
+from org.orekit.frames import FramesFactory
+from org.orekit.time import AbsoluteDate, TimeScalesFactory
+
+def preprocess(scc):
+    """ get corrected COEs by feeding TLE file into SGP4, then output COEs and TLE into a csv
     :param scc: str
-    :return: list
+    :return: nothing, outputs csv of preprocessed data for analysis and feeding into model
     """
-    with open(Path(f'data/maneuver_timestamps/{scc}.yaml'), 'r') as stream:
-        maneuvers = yaml.load(stream, Loader=yaml.FullLoader)
-    return maneuvers['manoeuvre_timestamps']
+    dict = {'epoch': [], # data loaded from TLEs into list values by key
+            'sma (km)': [],
+            'eccentricity': [],
+            'inclination (deg)': [],
+            'arg perigee (deg)': [],
+            'raan (deg)': [],
+            'mean anomaly (deg)': [],
+            'TLE line 1': [],
+            'TLE line 2': [],
+            'maneuvered': []} # 1 = last TLE prior to a maneuver, 0 = no maneuver
 
-def preprocess(scc, csv):
-    # dump ELSET data from JSON into df
-    df = pd.read_json(Path(f'data/ELSET/{scc}.json'), convert_dates=['EPOCH'])
-    features = ['EPOCH',
-                'ECCENTRICITY',
-                'INCLINATION',
-                'RA_OF_ASC_NODE',
-                'ARG_OF_PERICENTER',
-                'SEMIMAJOR_AXIS',
-                'MEAN_ANOMALY']
-    non_features = [i for i in list(df) if i not in features]
-    df.drop(non_features, axis=1, inplace=True)
-    df.reset_index(drop=True, inplace=True)
+    file = open(Path(f'data/tle/{scc}.tle'),'r')
+    content = file.read().splitlines()
+    for i in range(0, len(content)-1, 2):
+        # get corrected COEs
+        tle_line1 = content[i]
+        tle_line2 = content[i+1]
+        keplerian = get_keplerian_orbit(tle_line1, tle_line2)
+
+        # append to lists in dict
+        dict['epoch'].append(keplerian.getDate())
+        dict['sma (km)'].append(keplerian.getA()/1000.)
+        dict['eccentricity'].append(keplerian.getE())
+        dict['inclination (deg)'].append(math.degrees(keplerian.getI()))
+        dict['arg perigee (deg)'].append(math.degrees(keplerian.getPerigeeArgument()))
+        dict['raan (deg)'].append(math.degrees(keplerian.getRightAscensionOfAscendingNode()))
+        dict['mean anomaly (deg)'].append(math.degrees(keplerian.getMeanAnomaly()))
+        dict['TLE line 1'].append(tle_line1)
+        dict['TLE line 2'].append(tle_line2)
 
     # get maneuver data
     maneuvers = get_maneuvers(scc)
-    maneuvers = [i for i in maneuvers if i > df['EPOCH'][0]] # remove timestamps prior to first ELSET
+    maneuvers = [i for i in maneuvers if i.isAfter(dict['epoch'][0])] # remove timestamps prior to first ELSET
 
     # add column labeling if sat maneuvered following given ELSET
-    maneuvered_feature = []
+    # could be refactored so everything is done within the loop in line 35
     try:
-        for index in range(df.shape[0]):
-            if  df['EPOCH'][index] < maneuvers[0] < df['EPOCH'][index + 1] and len(maneuvers) > 0: # find ELSET immediately prior to maneuver
-                maneuvered_feature.append(1)
+        for index in range(len(dict['epoch'])):
+            if maneuvers[0].isBetween(dict['epoch'][index], dict['epoch'][index + 1]) and len(maneuvers) > 0: # find ELSET immediately prior to maneuver
+                dict['maneuvered'].append(1)
                 maneuvers.pop(0)
             else:
-                maneuvered_feature.append(0)
+                dict['maneuvered'].append(0)
     except IndexError: # reached last maneuver, fill rest of column with 0
-        for index in range(df.shape[0] - len(maneuvered_feature)):
-            maneuvered_feature.append(0)
-    df['MANEUVERED'] = maneuvered_feature
+        for index in range(len(dict['epoch']) - len(dict['maneuvered'])):
+            dict['maneuvered'].append(0)
 
-    if csv is True:
-        df.to_csv(Path(f'data/preprocessed/{scc}.csv'), index=False)
-    return df
+    df_final = pd.DataFrame.from_dict(dict)
+    df_final.to_csv(Path(f'data/preprocessed/{scc}.csv'), index=False)
 
-def get_tle(scc):
-    """ Extract TLEs from a json containing ELSET data
+def get_maneuvers(scc):
+    """ pull maneuver data from yaml and return as list of orekit AbsoluteDate instances
     :param scc: str
-    :return: two-column df containing TLEs
+    :return: list of AbsoluteDate instances
     """
-    df = pd.read_json(Path(f'data/ELSET/{scc}.json'), convert_dates=['EPOCH'])
-    return df[['TLE_LINE1', 'TLE_LINE2']]
+    with open(Path(f'data/maneuver_timestamps/{scc}.yaml'), 'r') as stream:
+        content = yaml.load(stream, Loader=yaml.FullLoader)
+    maneuvers = content['manoeuvre_timestamps']
+    maneuvers_processed = []
+    for i in maneuvers:
+        orekit_date = AbsoluteDate(i.year,
+                                   i.month,
+                                   i.day,
+                                   i.hour,
+                                   i.minute,
+                                   float(i.second),
+                                   TimeScalesFactory.getUTC())
+        maneuvers_processed.append(orekit_date)
+    return maneuvers_processed
+
+def get_last_tle(scc):
+    """ Extract most recent TLE from a json containing ELSET data
+    :param scc: str
+    :return: list containing TLE
+    """
+    df = pd.read_json(Path(f'data/tle/{scc}.json'), convert_dates=['EPOCH'])
+    tle = df[['TLE_LINE1', 'TLE_LINE2']].iloc[[-1]]
+    return tle.values.tolist()[0]
+
+def get_keplerian_orbit(tle_line1, tle_line2):
+    """ Feed TLE into SGP4, then output KeplerianOrbit instance in the inertial frame
+    :param tle_line1: str
+    :param tle_line2: str
+    :return: KeplerianOrbit instance
+    """
+    tle = TLE(tle_line1, tle_line2)
+    eme2000 = FramesFactory.getEME2000()
+    propagator = TLEPropagator.selectExtrapolator(tle, eme2000) # DSST works best in inertial frame
+    orbit = propagator.propagate(tle.getDate()).getOrbit()
+    orbit = OrbitType.KEPLERIAN.convertType(orbit) # returns only regular orbit
+    return KeplerianOrbit.cast_(orbit) # cast to KeplerianOrbit
